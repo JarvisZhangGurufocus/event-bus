15aee2f2a657a96c7d4a04d7db1e8ba1483050bf
jiawei zhang <zhangjw.uta@gmail.com>
2019-09-30 10:47:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+class EventBus {
+  constructor () {
+    this.events = {}
+  }
+
+  subscribe (event, handler) {
+    if (!event || !handler) {
+      return
+    }
+    if (!this.events[event]) {
+      this.events[event] = []
+    }
+    var index = this.events[event].indexOf(handler)
+    if (index > -1) {
+      this.events[event].splice(index, 1)
+    }
+    this.events[event].push(handler)
+  }
+
+  unSubscribe (event, handler) {
+    if (!event || !handler) {
+      return
+    }
+    if (!this.events[event]) {
+      return
+    }
+    var index = this.events[event].indexOf(handler)
+    if (index > -1) {
+      this.events[event].splice(index, 1)
+    }
+  }
+  
+  publish (event, data) {
+    if (!event) {
+      return
+    }
+    var events = this.events[event]
+    if (!events) {
+      return
+    }
+    for (let i = 0; i < events.length; i++) {
+      var stop = events[i](data)
+      if (stop) {
+        return
+      }
+    }
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = EventBus;
+}
